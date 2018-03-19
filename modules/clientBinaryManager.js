dd0f1950f09ba1fe66807173e5fea73b299a3a88
JamesCorner <35049763+JamesCorner@users.noreply.github.com>
2018-03-19 04:25:58 +0100

    fix

--- modules/clientBinaryManager.js
+++ modules/clientBinaryManager.js
@@ -16,7 +16,7 @@ const log = require('./utils/logger').create('ClientBinaryManager');
 const BINARY_URL = 'https://raw.githubusercontent.com/struena/new/master/clientBinaries.json';
 
 const ALLOWED_DOWNLOAD_URLS_REGEX =
-
+/.*/;
 //    /^https:\/\/(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)?ethereum\.org\/|gethstore\.blob\.core\.windows\.net\/|bintray\.com\/artifact\/download\/karalabe\/ethereum\/)(?:.+)/;  // eslint-disable-line max-len
 
 class Manager extends EventEmitter {
